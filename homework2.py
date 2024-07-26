d2995959fd60aa7fa0b72515ca0b4e4921903b33
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-07-26 15:17:54 +0300

    homework2.py

---
+++ homework2.py
@@ -0,0 +1,13 @@
+kolichestvo_vipolnennih_DZ = 12
+Kolichestvo_zatrachennih_chasov = 1.5
+nazvaniye_kursa = "Pyton"
+vremya_na_odno_zadaniye = Kolichestvo_zatrachennih_chasov / kolichestvo_vipolnennih_DZ
+print ("Курc: ", nazvaniye_kursa,",","всего задач:", kolichestvo_vipolnennih_DZ,",", "затрачено часов: ", Kolichestvo_zatrachennih_chasov,",", "среднее время выполнения ", vremya_na_odno_zadaniye,".")
+
+#Напишите 4 переменных которые буду обозначать следующие данные:
+#Количество выполненных ДЗ (запишите значение 12)
+#Количество затраченных часов (запишите значение 1.5)
+#Название курса (запишите значение 'Python')
+#Время на одно задание (вычислить используя 1 и 2 переменные)
+#Выведите на экран(в консоль), используя переменные, следующую строку:
+#Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
