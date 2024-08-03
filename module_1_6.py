4a2acb142e477c15ef97fef796d4ab4b89f8aa55
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-08-03 11:58:21 +0300

    Create module_1_6.py

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {alex : 2000, dmitry : 1990, anton : 1980}
+print(my_dict)
+print("печатаем одно значение", my_dict[alex])
+Print("не существующее значение", my_dict.get[alexander])
+
+my_dict.update{kostya : 2001, masha : 2002}
+my_dict.pop[kostya]
+print (my_dict[masha])
+print("измененный словарь", my_dict)
+
+my_set = {1, "odin", 1.5, 1, "odin"}
+print (my_set)
+my_set.add(2, "dva")
+my_set.discard (1)
+print("новое множество", my_set)
