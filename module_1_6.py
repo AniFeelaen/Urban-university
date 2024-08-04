45344e7d06cbcf52b799689e0b31ff0a21dc910d
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-08-04 12:08:06 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -3,13 +3,13 @@ print(my_dict)
 print("печатаем одно значение", my_dict[alex])
 Print("не существующее значение", my_dict.get[alexander])
 
-my_dict.update{kostya : 2001, masha : 2002}
-my_dict.pop[kostya]
+my_dict.update ({kostya : 2001, masha : 2002})
+a = my_dict.pop[kostya] #выбираем лишний элемент из словаря
 print (my_dict[masha])
 print("измененный словарь", my_dict)
 
 my_set = {1, "odin", 1.5, 1, "odin"}
 print (my_set)
 my_set.add(2, "dva")
-my_set.discard (1)
+my_set.discard (1) #убираем элемент из множества
 print("новое множество", my_set)
