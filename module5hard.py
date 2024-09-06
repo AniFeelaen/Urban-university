660a556bb7977c85b38e82bc27c1de57755c36e6
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-09-06 03:19:04 +0300

    начальная
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module5hard.py
+++ module5hard.py
@@ -1,24 +1,30 @@
-class UrTube:
-    def __new__(cls, *args, **kwargs):
-        # cls.houses_history.append(args[0])
-        return object.__new__(cls)              #обязательное условие возврата значений
-    def __init__ (self, name, floors):
-        self.name = name
-        self.number_of_floors = floors
-        
-        
-class Video:
-    def __init__ (self, title, duration, time_now, adult_mode):
+class Video:   
+    def __init__ (self, title: str, duration: int, time_now, adult_mode = False):
         self.title = title
         self.duration = duration
-        self.time_now = time_now
-        self.adult_mode = adult_mode
-           
-        
-        
-        
+        self.time_now = 0
+        self.adult_mode = adult_mode                      
 class User:
-    def __init__ (self, name, password, age):
-        self.name = name
-        self.password = password
+    def __init__ (self, name: str, password: int, age: int):
+        self.name = []
+        self.password = hash(password)
         self.age = age
+class UrTube:
+    def __init__ (self, users, videos, current_user):
+        self.users = users
+        self.videos = videos
+        self.current_user = current_user
+    
+    def log_in(nickname,password):
+        global current_user, users
+        if nickname in users and password == hash(password):
+            current_user == nickname
+            return True
+        return False
+    def register (self, name, password, age):
+        if name not in self.name:
+            self.name.append(name)
+        else:
+            print (f"Пользователь {nickname} уже существует")
+        return True
+    
