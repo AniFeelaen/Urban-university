b5f7c42a72d8a50b35ac3cb2b2419ea22503bef1
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-09-19 16:40:21 +0300

    1
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_7_1.py
+++ module_7_1.py
@@ -3,34 +3,40 @@ class Product:
         self.name = name
         self.weight = weight
         self.category = category
-        
+
     def __str__(self) -> str:
-        return f'{self.name}, {self.weight}, {self.category}\n'
+        return f'{self.name}, {self.weight}, {self.category}'
+
 class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
-        
+
     def get_products(self):
-        open(self.__file_name, 'r')
-        self.__file_name.close
-        return self.__file_name.read
-    
+        file = open(self.__file_name, 'r')
+        content = file.read()
+        file.close()
+        return content
+
     def add(self, *products):
-        all_products = self.get_products
+        all_products = self.get_products()
+        added_names = []
         for product in products:
-            if product in all_products:
-                print(f'Продукт {self.name} уже есть в магазине')
+            name = product.name
+            if name in added_names or name in all_products:
+                print(f'Продукт {name} уже есть в магазине')
             else:
-                open(self.__file_name, 'a')
-                self.__file_name.write(f'{self.name}, {self.weight}, {self.category}\n')
-                self.__file_name.close
-            
+                file = open(self.__file_name, 'a')
+                file.write(product.__str__() + '\n')
+                added_names.append(name)
+                file.close
+
+# Пример использования
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
--- musor.py
+++ musor.py
@@ -5,22 +5,39 @@ class Product:
         self.category = category
 
     def __str__(self) -> str:
-        return f'{self.name}, {self.weight}, {self.category}\n'
+        return f'{self.name}, {self.weight}, {self.category}'
 
 class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
 
     def get_products(self):
-        with open(self.__file_name, 'r') as file:
-            return file.read().splitlines()
+        file = open(self.__file_name, 'r')
+        content = file.read()
+        file.close()
+        return content
 
     def add(self, *products):
-        existing_products = self.get_products()
+        all_products = self.get_products()
+        added_names = []
         for product in products:
-            if product.name in map(lambda p: p.name, existing_products):
-                print(f'Продукт {product.name} уже есть в магазине')
+            name = product.name
+            if name in added_names or name in all_products:
+                print(f'Продукт {name} уже есть в магазине')
             else:
-                with open(self.__file_name, 'a+') as file:
-                    file.write(f'{product}\n')
-                    
+                file = open(self.__file_name, 'a')
+                file.write(product.__str__() + '\n')
+                added_names.append(name)
+                file.close
+
+# Пример использования
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
