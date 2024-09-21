8c11971d56f37235345d09368af5636998f93d69
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-09-21 21:25:15 +0300

    1
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_7_3.py
+++ module_7_3.py
@@ -6,26 +6,26 @@ class WordsFinder:
         all_words = {}
         for file_name in self.file_names:
             with open (file_name, 'r', encoding= 'utf-8') as file:
-                # words = file.read().lower().replace([',', '.', '=', '!', '?', ';', ':', ' - '], '')
-                words = file.read().lower().replace(',', '').replace('.', '').replace('=', '').replace('!', '').replace('?', '').replace(';', '').replace(':', '').replace(' - ', '')
+                # words = file.read().lower().replace([',', '.', '=', '!', '?', ';', ':', ' - '], '') список использовать нельзя, в конце не забываем. split. чтобы каждое слово вошло в словарь
+                words = file.read().lower().replace(',', '').replace('.', '').replace('=', '').replace('!', '').replace('?', '').replace(';', '').replace(':', '').replace(' - ', '').split()
                 all_words[file_name] = words
         return all_words
+    
     def find(self, word: str):
         all_words = self.get_all_words()
         positions = {}
+        word = word.lower() #обязательно преобразуем искомое слово в нижний регистр чтобы им (регистром) пренебречь.
         for file_name, words in all_words.items():
-            try:
-                index = words.index(word)
+                index = words.index(word)   #используем index - определяет индекс первого встреченного
                 positions[file_name] = index
-            except ValueError:
-                pass
         return positions
 
     def count(self, word: str):
         all_words = self.get_all_words()
         counts = {}
+        word = word.lower()  #обязательно преобразуем искомое слово в нижний регистр чтобы им (регистром) пренебречь.
         for file_name, words in all_words.items():
-            count = words.count(word)
+            count = words.count(word) # используем count - считаем сколько всего слов
             counts[file_name] = count
         return counts
     
