68004dd73e42792d2f8f51d8bcd22b77d637311b
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-09-26 13:50:27 +0300

    1
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_8_1.py
+++ module_8_1.py
@@ -1,9 +1,10 @@
 
 def add_everything_up(a, b):
     try: 
-       return round(a + b, 3)
+       return round(a + b, 3)  #выполняем сложение чисел если все окей + округляем
     except TypeError as exc:
-        return str(a)+ str(b)
+       return str(a) + str(b)  # если есть несочетание типов между собой то выполняем сложение строк
+        # return f'{a}{b}'
         
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
