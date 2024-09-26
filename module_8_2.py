8ac6420bd078c2486f583818162d2d823afb3a8d
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-09-26 14:46:13 +0300

    финал
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

---
+++ module_8_2.py
@@ -0,0 +1,29 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for number in numbers:
+        try:
+            result += number
+        except TypeError:
+            incorrect_data += 1 
+            print(f'Некорректный тип данных для подсчёта суммы - {number}') #каждый раз если строка а не число, получим пример неправильного типа данных
+    return result, incorrect_data          
+    
+def calculate_average(numbers):
+    try:
+        summa, incorrect_data = personal_sum(numbers) # в кортеже из суммы и некорректных данных используем только сумму
+        average = summa / len(numbers)
+        return average
+    except ZeroDivisionError:
+        print('Количество элементов в коллекции равно нулю')
+        return 0
+    except TypeError:
+        print ('В numbers записан некорректный тип данных')
+        return None
+        
+    
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать    
+print(f'Результат 5: {calculate_average([])}')# передаем пустую коллекцию
--- musor.py
+++ musor.py
@@ -1,39 +1,25 @@
-import re
-from typing import Dict, List, Tuple
+def personal_sum(numbers):
+    incorrect_data = 0
+    result = 0
+    for number in numbers:
+        try:
+            result += number
+        except TypeError:
+            incorrect_data += 1
+    return result, incorrect_data
 
-class WordsFinder:
-    def __init__(self, *file_names: str):
-        self.file_names = list(file_names)
-
-    def get_all_words(self) -> Dict[str, List[str]]:
-        all_words = {}
-        for filename in self.file_names:
-            with open(filename, 'r', encoding='utf-8') as file:
-                words = file.read().lower().replace('\n', '').replace('.', '').replace(',', '').replace('=', '').replace('!', '').replace('?', '').replace(':', '').replace(';', '').replace(' - ', '')
-                words = re.sub(r'\s+', ' ', words).strip().split()
-                all_words[filename] = words
-        return all_words
-
-    def find(self, word: str) -> Dict[str, int]:
-        all_words = self.get_all_words()
-        positions = {}
-        for filename, words in all_words.items():
-            try:
-                index = words.index(word)
-                positions[filename] = index
-            except ValueError:
-                pass
-        return positions
-
-    def count(self, word: str) -> Dict[str, int]:
-        all_words = self.get_all_words()
-        counts = {}
-        for filename, words in all_words.items():
-            count = words.count(word)
-            counts[filename] = count
-        return counts
-
-finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+def calculate_average(numbers):
+    try:
+        sum_, incorrect_data = personal_sum(numbers)
+        average = sum_ / len(numbers)
+        return average
+    except ZeroDivisionError:
+        print('Количество элементов в коллекции равно нулю. Среднее арифметическое равно 0.')
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных. Среднее арифметическое не может быть рассчитано.')
+        return None
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
