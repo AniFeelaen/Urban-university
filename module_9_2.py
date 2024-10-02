2b163c0ee64ec678f5768e2cefd43737115b043d
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-10-02 18:59:58 +0300

    1
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

---
+++ module_9_2.py
@@ -0,0 +1,12 @@
+
+
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(x) for x in first_strings if len(x) >= 5]
+second_result = [ (x , y) for x in first_strings for y in second_strings if len(x) == len(y)]#ставим (х,y) чтобы вывести пару
+third_result = {x: len(x) for x in (first_strings + second_strings) if len(x) % 2 == 0} #просто складываем 2 строки, можно добавить str чтобы складывались строки а не значения
+
+print(first_result)
+print(second_result)
+print(third_result)
