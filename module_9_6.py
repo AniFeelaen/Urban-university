a5585bf8d4793134a2cc74b32d7f7eb5bcace9da
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-10-09 02:10:46 +0300

    3
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_9_6.py
+++ module_9_6.py
@@ -1,5 +1,5 @@
 def all_variants(text):
-    for i in range(len(text)): #проходимся по списку а потом по списку после нашего элемента и записываем
+    for i in range(len(text)): #проходимся по списку после нашего элемента и записываем
         for j in range(i,len(text)): 
             yield text[i:j+1]  
 
