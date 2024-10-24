ef45f7e0685efa751dc2778ea07b95e2b1a3f73d
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-10-24 12:45:58 +0300

    2
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_10_5.py
+++ module_10_5.py
@@ -8,3 +8,23 @@ if __name__ == '__main__':
         results = pool.map(square, range(10))
         
     print(results)
+    
+    
+    
+from multiprocessing import Process, Event
+
+def wait_for_event(e):
+    print('Process started')
+    e.wait()
+    print('Event received')
+
+if __name__ == '__main__':
+    event = Event()
+    
+    p1 = Process(target=wait_for_event, args=(event,))
+    p1.start()
+    
+    input('Press Enter to set the event...')
+    event.set()
+    
+    p1.join()
