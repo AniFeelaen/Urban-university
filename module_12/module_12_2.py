5b2068030fed00c120e642de92032612fd694072
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-11-10 16:43:24 +0300

    23
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_12/module_12_2.py
+++ module_12/module_12_2.py
@@ -33,16 +33,53 @@ class Tournament:
             for participant in self.participants:
                 participant.run()
                 if participant.distance >= self.full_distance:
-                    finishers[place] = participant
+                    finishers[place] = participant.name #participant.NAME - получаем имя а не объект для сравнения и сравниваем его потом с "Ник"
                     place += 1
                     self.participants.remove(participant)
 
         return finishers
     
     
+class TournamentTest(unittest.TestCase):
     
+    # Создаём атрибут класса для хранения результатов всех тестов
+    all_results = {}
     
-    
-    
-if __name__ == "__main__":
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+        
+    def setUp(self):
+        # Создаем трех участников забега
+        self.usain = Runner("Усэйн", 10)
+        self.andrey = Runner("Андрей", 9)
+        self.nik = Runner("Ник", 3)
+        
+    @classmethod
+    def tearDownClass(cls):
+        for key, value in cls.all_results.items():
+            print(f"{key}: {value}")  #выводим место и имя
+            
+    def test_usain_and_nik(self):
+        tournament = Tournament(90, self.usain, self.nik)
+        results = tournament.start()
+        last_runner = max(results.keys())
+        self.assertEqual(results[last_runner], 'Ник') #сравниваем результат последнего раннера с самым медленным(ником)
+        self.__class__.all_results['test_usain_and_nik'] = results
+        
+    def test_andrey_and_nik(self):
+        tournament = Tournament(90, self.andrey, self.nik)
+        results = tournament.start()
+        last_runner = max(results.keys())
+        self.assertEqual(results[last_runner], 'Ник')
+        self.__class__.all_results['test_andrey_and_nik'] = results
+        
+    def test_all_three_runners(self):
+        tournament = Tournament(90, self.usain, self.andrey, self.nik)
+        results = tournament.start()
+        last_runner = max(results.keys())
+        self.assertEqual(results[last_runner], 'Ник')
+        self.__class__.all_results['test_all_three_runners'] = results
+
+if __name__ == '__main__':
     unittest.main()
