bd7fd78facfc8b77eaabf0b8dc3011c90a9504ef
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-11-27 17:47:03 +0300

    bot answer
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_13/module_13_3.py
+++ module_13/module_13_3.py
@@ -1,13 +1,29 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+import bot_key
 
-if text not in range(6, 80):
-    await bot.send_message(
-        chat_id=message.chat_id,
-        text = 'Подозрительный возраст'
-    )
+storage = MemoryStorage()
+bot = Bot(token= bot_key.TOKEN)
+
+dp = Dispatcher(bot, storage= storage)
+
+# @dp.message_handler(text = ['Urban', 'ff'])
+# async def urban_message(message):
+#     print('Urban message')
+    
+@dp.message_handler(commands = ["start"] )
+async def start (message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    # await message.reply
+@dp.message_handler()
+async def all_message (message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates= True)
+
     
     
     
     
-ReplyKeyboardMarkup(
-    
-)
---
+++ module_13/module_13_4.py
@@ -0,0 +1,11 @@
+
+
+if text not in range(6, 80):
+    await bot.send_message(
+        chat_id=message.chat_id,
+        text = 'Подозрительный возраст'
+    )
+    
+    ReplyKeyboardMarkup(
+    
+)
