b1c872a3ab053227eea3bfa106c7eb203ee876bc
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-11-27 17:29:57 +0300

    2
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

---
+++ module_13/module_13_3.py
@@ -0,0 +1,13 @@
+
+if text not in range(6, 80):
+    await bot.send_message(
+        chat_id=message.chat_id,
+        text = 'Подозрительный возраст'
+    )
+    
+    
+    
+    
+ReplyKeyboardMarkup(
+    
+)
