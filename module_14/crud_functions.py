e54c7cf3ebb3aeaa108da717c7f26356ff5ffcf3
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-12-18 18:33:19 +0300

    TgbotchangeswithDB
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

---
+++ module_14/crud_functions.py
@@ -0,0 +1,35 @@
+import sqlite3
+import random
+
+# Создаем подключение к базе данных
+def initiate_db():
+    connect = sqlite3.connect('products.db')
+    cursor = connect.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Products (
+            id INTEGER PRIMARY KEY,
+            title TEXT NOT NULL,
+            description TEXT,
+            price INTEGER NOT NULL
+        )
+    ''')
+    connect.commit
+    connect.close
+# def add_user(user_id, username, firstname):
+#     check_user = cursor.execute("SELECT * FROM  Users where id=?", (user_id))
+    
+#     if check_user.fetchone() in None:
+#         cursor.execute(f'''
+#     INSERT INTO Users VALUES('{user_id}', '{username}', '{firstname}', 0)                   
+#     ''')
+#     connect.commit()
+def get_all_products():
+    connect = sqlite3.connect('products.db')
+    cursor = connect.cursor()
+    
+    cursor.execute("SELECT * FROM Products")
+    products = cursor.fetchall()
+    
+    connect.close()
+    return products    
