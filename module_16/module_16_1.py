30403e856776c33751ef00645280c7579e6879dd
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-12-25 15:30:35 +0300

    2
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -1,21 +1,23 @@
 from fastapi import *
 app = FastAPI()
 
-@app.get("/user/admin")
-async def read_admin() -> str:
-    return ("Вы вошли как администратор")
-
-@app.get("/user/{user_id}")
-async def read_user_id(user_id: int) -> str:
-    return (f"Вы вошли как пользователь № {user_id}")
+@app.get("/")
+async def read_root() -> str:
+    return ("Главная страница")
 
 @app.get("/user")
 async def read_username_age(username: str, age: int) -> str:
     return (f"Информация о пользователе. Имя: {username}, Возраст: {age}")
 
-@app.get("/")
-async def read_root() -> str:
-    return ("Главная страница")
+@app.get("/user/{user_id}")
+async def read_user_id(user_id: int) -> str:
+    return (f"Вы вошли как пользователь № {user_id}")
+
+@app.get("/user/admin")
+async def read_admin() -> str:
+    return ("Вы вошли как администратор")
+
+
 # uvicorn Urban-university.module_16.module_16_1:app
 
 # if __name__ == '__main__':
