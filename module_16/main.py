26b68d508907593e04b5ef02c0e4c9f6bbbe4097
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2024-12-25 14:47:09 +0300

    1
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- module_16/main.py
+++ module_16/main.py
@@ -1,18 +1,19 @@
-from fastapi import FastAPI
+from fastapi import *
 app = FastAPI()
 
 @app.get("/user/{user_id}")
-async def read_user_id(user_id: int) -> dict:
+async def read_user_id(user_id: int) -> str:
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 @app.get("/user/admin")
-async def read_admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def read_admin() -> str:
+    return {"Вы вошли как администратор"}
 
 @app.get("/user")
-async def read_username_age(username: str, age: int) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def read_username_age(username: str, age: int) -> str:
+    return {f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
 @app.get("/")
 async def read_root() -> dict:
     return {"message": "Главная страница"}
+# Urban-university.module_16.main:app
--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -1,21 +1,22 @@
-from fastapi import FastAPI
+from fastapi import *
 app = FastAPI()
 
-@app.get("/user/{user_id}")
-async def read_user_id(user_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
-
 @app.get("/user/admin")
-async def read_admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def read_admin() -> str:
+    return ("Вы вошли как администратор")
+
+@app.get("/user/{user_id}")
+async def read_user_id(user_id: int) -> str:
+    return (f"Вы вошли как пользователь № {user_id}")
 
 @app.get("/user")
-async def read_username_age(username: str, age: int) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def read_username_age(username: str, age: int) -> str:
+    return (f"Информация о пользователе. Имя: {username}, Возраст: {age}")
 
 @app.get("/")
-async def read_root() -> dict:
-    return {"message": "Главная страница"}
+async def read_root() -> str:
+    return ("Главная страница")
+# uvicorn Urban-university.module_16.module_16_1:app
 
 # if __name__ == '__main__':
 #     app.run()
@@ -23,3 +24,4 @@ async def read_root() -> dict:
 # cd Urban_university/main.py    
 # >> python -m uvicorn main:app
 # uvicorn main:app --reload
+# Urban-university.module_16.main:app --reload
