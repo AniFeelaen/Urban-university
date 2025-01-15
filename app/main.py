4aba464ee651684928b75ea07bcafe12fe6041a8
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2025-01-15 19:34:38 +0300

    2
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 
 
-# uvicorn UrbanUniversity.app.main:app
+# uvicorn Urban-university.app.main:app
 from fastapi import FastAPI
 # from routers.task import router as task_router
 # from routers.user import router as user_router
-from routers import task
+
 from routers import user
-from app.routers import task
-from app.routers import user
+from routers import task
+# from app.routers import 
 
 
 
@@ -17,8 +17,6 @@ app = FastAPI()
 def welcome_message():
     return {"message": "Welcome to Taskmanager"}
 
-# app.include_router(task_router, prefix="/task", tags=["task"])
-# app.include_router(user_router, prefix="/user", tags=["user"])
 # app.include_router(category.router)
 app.include_router(task.router)
 app.include_router(user.router)
