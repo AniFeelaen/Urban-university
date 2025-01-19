0583e16502463de63d77ef27b9a8953fdec1ff4e
AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>
2025-01-19 17:18:49 +0300

    2
    Signed-off-by: AniFeelaen <47817971+AniFeelaen@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -3,14 +3,9 @@
 # uvicorn Urban-university.app.main:app
 # uvicorn app.main:app
 from fastapi import FastAPI
-# from routers.task import router as task_router
-# from routers.user import router as user_router
-
 from routers import user
 from routers import task
 # from app.routers import 
-
-
 
 app = FastAPI()
 
